0ec1d64c8a85cf3882ece2e0e71fbe5d1bf33243
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2023-09-29 16:19:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function volume_sphere() {
-    //Write your code here
+	let r = document.getElementById("radius").value
+	
+	let v = document.getElementById("volume")
+	  const pi = Math.PI;
+  const volume = (4 / 3) * pi * Math.pow(r, 3);
+  v.value = volume;
   
 } 
 
