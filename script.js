7302a5b7ca80bd1f9dd03f44445793cf421fb5c8
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2024-08-18 22:18:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-function volume_sphere(e) {
-	e.preventDefault()
-	let r = document.getElementById("radius").value
-		let result = document.getElementById("result")
-
-	let v = document.getElementById("volume")
-	  const pi = Math.PI;
-  const volume = (4 / 3) * pi * Math.pow(r, 3);
-  v.value = volume.toFixed(4);
-  result.innerText = volume.toFixed(4)
-} 
-
+function volume_sphere() {
+    let r = document.getElementById("radius").value;
+    
+    r = parseFloat(r);
+    
+    let v = document.getElementById("volume");
+    let result = document.getElementById("result");
+    
+    if (isNaN(r) || r < 0) {
+        v.value = "NaN";
+        result.innerText = "NaN";
+    } else {
+        const pi = Math.PI;
+        const volume = (4 / 3) * pi * Math.pow(r, 3);
+        
+        const roundedVolume = volume.toFixed(4);
+        v.value = roundedVolume;
+        result.innerText = roundedVolume;
+    }
+}
