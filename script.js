9244fd16a56251dac1c9bb2af91bb2b62dcd2fd3
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2024-08-18 22:17:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-function volume_sphere() {
+function volume_sphere(e) {
+	e.preventDefault()
 	let r = document.getElementById("radius").value
-	
+		let result = document.getElementById("result")
+
 	let v = document.getElementById("volume")
 	  const pi = Math.PI;
   const volume = (4 / 3) * pi * Math.pow(r, 3);
   v.value = volume.toFixed(4);
-  
+  result.innerText = volume.toFixed(4)
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
